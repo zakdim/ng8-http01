62824dfbe8e8f085e17e02e73eead4c61424c3b1
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-12-17 22:30:20 -0500

    Multiple interceptors
    section 18, lecture 274

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,22 +1,13 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
    
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('Request is on its way!');
-        console.log(`${req.method} ${req.url}`);
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
     
 }
---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpEventType } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('Outgoing request');
+        console.log(`${req.method} ${req.url}`);
+        console.log(req.headers);
+        return next.handle(req).pipe(tap(event => {
+            if (event.type === HttpEventType.Response) {
+                console.log('Incoming response');
+                console.log(event.body);
+            }
+        }));
+    }
+
+}
